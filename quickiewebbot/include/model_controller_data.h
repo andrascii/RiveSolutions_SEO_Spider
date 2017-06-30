ed280a61488904bd57c3fdcc9c9361c1f8705c43
Andrew <logforregs@gmail.com>
2017-06-30 19:45:26 +0300

    changed model controller data

--- quickiewebbot/include/model_controller_data.h
+++ quickiewebbot/include/model_controller_data.h
@@ -32,22 +32,60 @@ private:
 	GuiQueueType* guiQueue(QueueType type) noexcept;
 
 private:
-	struct CrawlerQueues
+	template <typename QueueType>
+	struct Queues
 	{
-		CrawlerQueueType internalUrlsQueue;
-		CrawlerQueueType crawledUrlsQueue;
-		CrawlerQueueType externalUrlsQueue;
+		// internal crawler url queues
+		QueueType internalUrlsQueue;
+		QueueType crawledUrlsQueue;
+		QueueType externalUrlsQueue;
+
+		// urls
+		QueueType upperCaseUrlsQueue;
+		QueueType nonAsciiCharacterUrlsQueue;
+		QueueType veryLongUrlsQueue;
+
+		// titles
+		QueueType emptyTitleUrlsQueue;
+		QueueType duplicatedTitleUrlsQueue;
+		QueueType veryLongTitleUrlsQueue;
+		QueueType veryShortTitleUrlsQueue;
+		QueueType duplicatedH1TitleUrlsQueue;
+		QueueType severalTitleUrlsQueue;
+
+		// meta descriptions
+		QueueType emptyMetaDescriptionUrlsQueue;
+		QueueType duplicatedMetaDescriptionUrlsQueue;
+		QueueType veryLongMetaDescriptionUrlsQueue;
+		QueueType veryShortMetaDescriptionUrlsQueue;
+		QueueType severalMetaDescriptionUrlsQueue;
+
+		// meta keywords
+		QueueType emptyMetaKeywordsUrlsQueue;
+		QueueType duplicatedMetaKeywordsUrlsQueue;
+		QueueType severalMetaKeywordsUrlsQueue;
+
+		// H1
+		QueueType missingH1UrlsQueue;
+		QueueType duplicatedH1UrlsQueue;
+		QueueType veryLongH1UrlsQueue;
+		QueueType severalH1UrlsQueue;
+
+		// H2
+		QueueType missingH2UrlsQueue;
+		QueueType duplicatedH2UrlsQueue;
+		QueueType veryLongH2UrlsQueue;
+		QueueType severalH2UrlsQueue;
+
+		// images
+		QueueType over100kbImagesQueue;
+		QueueType missingAltTextImagesQueue;
+		QueueType veryLongAltTextImagesQueue;
 	};
 
-	struct GuiQueues
-	{
-		GuiQueueType internalUrlsQueue;
-		GuiQueueType crawledUrlsQueue;
-		GuiQueueType externalUrlsQueue;
-	};
-
-	CrawlerQueues m_crawlerQueues;
-	GuiQueues m_guiQueues;
+private:
+	Queues<CrawlerQueueType> m_crawlerQueues;
+	Queues<GuiQueueType> m_guiQueues;
 };
 
 }
