180d3ffa6d67d207945e0ff9c4005dbd97cffd3f
andrey_ascii <pugachev.andrey.work@gmail.com>
2017-07-04 22:04:30 +0300

    Initialized new project for crawler

---
+++ crawler/include/stdafx.h
@@ -0,0 +1,75 @@
+#pragma once
+
+//
+// C/C++
+//
+#include <vector>
+#include <deque>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <condition_variable>
+#include <thread>
+#include <mutex>
+#include <future>
+#include <chrono>
+#include <string>
+#include <type_traits>
+#include <typeinfo>
+#include <atomic>
+#include <memory>
+#include <utility>
+#include <cassert>
+#include <functional>
+
+//
+// boost
+//
+#include <boost/functional/hash.hpp>
+
+//
+// Qt
+//
+#include <QObject>
+#include <QThread>
+#include <QWidget>
+#include <QDialog>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QApplication>
+#include <QMessageBox>
+#include <QTextCodec>
+#include <QAbstractListModel>
+#include <QKeyEvent>
+#include <QMetaObject>
+#include <QMetaMethod>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkRequest>
+#include <QtNetwork/QNetworkReply>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QPainter>
+#include <QTimer>
+#include <QAbstractItemDelegate>
+
+using namespace std::chrono_literals;
+using std::size_t;
+
+#define myApp Application::instance()
+
+#define STRING(Any) #Any
+
+#if defined(NDEBUG)
+#define PRODUCTION
+#endif
+
+#if !defined(PRODUCTION)
+#define DEBUG
+#endif
+
+#if !defined(PRODUCTION)
+#define VERIFY(Connection) assert(Connection)
+#else
+#define VERIFY(Connection) Connection
+#endif
---
+++ crawler/src/crawler_controller.cpp
@@ -0,0 +1,11 @@
+#include "crawler_controller.h"
+#include "crawler_page_info_acceptor.h"
+
+namespace QuickieWebBot
+{
+
+CrawlerController::CrawlerController()
+{
+}
+
+}
