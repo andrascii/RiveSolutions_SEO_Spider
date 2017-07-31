e700791ef4caab279ec6c7b0008ecab3469250fe
andrey_ascii <pugachev.andrey.work@gmail.com>
2017-07-31 23:33:25 +0300

    Рефактор

--- quickiewebbot/include/page_info_storage_model.h
+++ quickiewebbot/include/page_info_storage_model.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "igrid_model.h"
+#include "page_info.h"
 
 namespace QuickieWebBot
 {
@@ -8,9 +9,20 @@ namespace QuickieWebBot
 class IGridViewResizeStrategy;
 class GridViewResizeStrategy;
 
+//
+//! This model intended for establish associate with some PageInfo storage 
+//! which placed in the DataCollection instance.
+//! Using this model you can customize PageInfo columns set.
+//
+
 class PageInfoStorageModel : public IGridModel
 {
 	Q_OBJECT
+
+	//
+	// If you want to add a model for only one PageInfo then you need
+	// to create a new derived type from IGridModel and customize it appropriately.
+	//
 
 public:
 	PageInfoStorageModel(QObject* parent = nullptr);
@@ -29,6 +41,8 @@ public:
 	virtual IGridViewResizeStrategy* resizeStrategy() const override;
 
 private:
+	QList<PageInfo::ItemType> m_pageInfoColumns;
+
 	std::unique_ptr<GridViewResizeStrategy> m_resizeStrategy;
 };
 
