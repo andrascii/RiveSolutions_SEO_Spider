5ab7bd39b168ff2a70fc00ef04e45dab13f9254b
andrey_ascii <pugachev.andrey.work@gmail.com>
2017-07-31 23:21:05 +0300

    Теперь билд собирается. Также Summary view начал отображаться нормально

--- quickiewebbot/include/igrid_model.h
+++ quickiewebbot/include/igrid_model.h
@@ -6,6 +6,13 @@ namespace QuickieWebBot
 class IGridModel : public QAbstractTableModel
 {
 public:
+	//
+	// ctor inheritance
+	//
+	using QAbstractTableModel::QAbstractTableModel;
+
+	virtual ~IGridModel() = default;
+
 	virtual IGridViewResizeStrategy* resizeStrategy() const = 0;
 };
 
