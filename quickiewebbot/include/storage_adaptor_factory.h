0a8baddc054cb3c53f4b59f9736a3d96329c5ef6
andrey_ascii <pugachev.andrey.work@gmail.com>
2017-08-05 16:48:42 +0300

    Добавлено задание допустимых колонок для каждого вида хранилища

--- quickiewebbot/include/storage_adaptor_factory.h
+++ quickiewebbot/include/storage_adaptor_factory.h
@@ -12,10 +12,14 @@ class StorageAdaptorFactory
 public:
 	StorageAdaptorFactory(DataCollection* dataCollection);
 
-	StorageAdaptor* create(DataCollection::StorageType type) const;
+	StorageAdaptor* create(DataCollection::StorageType type);
+
+protected:
+	void setupAvailableColumns(StorageAdaptor* storageAdaptor, DataCollection::StorageType type) const;
 
 private:
 	DataCollection* m_dataCollection;
+	std::map<DataCollection::StorageType, StorageAdaptor*> m_storageAdaptors;
 };
 
 }
