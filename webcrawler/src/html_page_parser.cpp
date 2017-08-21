9fcf8128a81c30f189a9634f1ae39547891f3fc9
andrey_ascii <pugachev.andrey.work@gmail.com>
2017-08-21 22:26:16 +0300

    вынесены две функции по определению и декодированию html страницы

--- webcrawler/src/html_page_parser.cpp
+++ webcrawler/src/html_page_parser.cpp
@@ -5,40 +5,6 @@
 namespace WebCrawler
 {
 
-class GumboOutputCreatorDestroyerGuard
-{
-public:
-	GumboOutputCreatorDestroyerGuard(const GumboOptions* options, const QByteArray& htmlPage)
-		: m_gumboOptions(options)
-		, m_gumboOutput(gumbo_parse_with_options(options, htmlPage.data(), htmlPage.size()))
-	{
-	}
-
-	~GumboOutputCreatorDestroyerGuard()
-	{
-		gumbo_destroy_output(m_gumboOptions, m_gumboOutput);
-	}
-
-	GumboOutputCreatorDestroyerGuard(const GumboOutputCreatorDestroyerGuard&) = delete;
-	GumboOutputCreatorDestroyerGuard(GumboOutputCreatorDestroyerGuard&&) = delete;
-	GumboOutputCreatorDestroyerGuard& operator=(const GumboOutputCreatorDestroyerGuard&) = delete;
-	GumboOutputCreatorDestroyerGuard& operator=(GumboOutputCreatorDestroyerGuard&&) = delete;
-
-	const GumboOutput* output() const noexcept
-	{
-		return m_gumboOutput;
-	}
-
-	GumboOutput* output() noexcept
-	{
-		return m_gumboOutput;
-	}
-
-private:
-	const GumboOptions* m_gumboOptions;
-	GumboOutput* m_gumboOutput;
-};
-
 
 void HtmlPageParser::addPageRawParser(std::shared_ptr<IPageParser> pageRawParser) noexcept
 {
@@ -51,8 +17,7 @@ void HtmlPageParser::parsePage(const QByteArray& htmlPage, PageRawPtr& pageRaw) 
 	// TODO: optimize this code because for now we need double parsing of the same page
 	//
 
-
-	QByteArray decodedHtmlPage = decodeHtmlPage(htmlPage);
+	QByteArray decodedHtmlPage = GumboParsingHelpers::decodeHtmlPage(htmlPage);
 	GumboOutputCreatorDestroyerGuard gumboOutput(&kGumboDefaultOptions, decodedHtmlPage);
 
 	for (const std::shared_ptr<IPageParser>& parser : m_parserPack)
@@ -90,80 +55,6 @@ void HtmlPageParser::parsePageUrlList(const GumboNode* node) noexcept
 		const GumboNode* child = static_cast<const GumboNode*>(children->data[i]);
 		parsePageUrlList(child);
 	}
-}
-
-
-QByteArray HtmlPageParser::identifyHtmlPageContentType(const QByteArray& htmlPage) const noexcept
-{
-	GumboOutputCreatorDestroyerGuard gumboOutput(&kGumboDefaultOptions, htmlPage);
-
-	GumboNode* head = GumboParsingHelpers::findSubNode(gumboOutput.output()->root, GUMBO_TAG_HEAD);
-	assert(head->type == GUMBO_NODE_ELEMENT && head->v.element.tag == GUMBO_TAG_HEAD);
-
-	std::vector<GumboNode*> metaTags = GumboParsingHelpers::subNodes(head, GUMBO_TAG_META);
-
-	for (unsigned i = 0; i < metaTags.size(); ++i)
-	{
-		GumboAttribute* contentAttribute = gumbo_get_attribute(&metaTags[i]->v.element.attributes, "content");
-
-		if (!contentAttribute)
-		{
-			continue;
-		}
-
-		GumboAttribute* metaHttpEquivAttribute = gumbo_get_attribute(&metaTags[i]->v.element.attributes, "http-equiv");
-
-		if (metaHttpEquivAttribute)
-		{
-			QString attributeValue = QString(metaHttpEquivAttribute->value).toLower();
-
-			if (attributeValue == "content-type")
-			{
-				return contentAttribute->value;
-			}
-		}
-	}
-
-	return QByteArray();
-}
-
-QByteArray HtmlPageParser::decodeHtmlPage(const QByteArray& htmlPage) const noexcept
-{
-	QByteArray decodedHtmlPage = htmlPage;
-	QByteArray contentType = identifyHtmlPageContentType(htmlPage);
-
-	QByteArray charset = contentType.right(contentType.size() - contentType.lastIndexOf("=") - 1);
-
-	if (charset.isEmpty())
-	{
-		QTextCodec* codecForHtml = QTextCodec::codecForHtml(decodedHtmlPage.data());
-
-		if (codecForHtml)
-		{
-			decodedHtmlPage = codecForHtml->toUnicode(decodedHtmlPage).toStdString().data();
-		}
-		else
-		{
-			ERRORLOG << "Cannot identify page encoding";
-		}
-	}
-	else
-	{
-		QTextCodec* codecForCharset = QTextCodec::codecForName(charset);
-
-		if (codecForCharset)
-		{
-			decodedHtmlPage = codecForCharset->toUnicode(decodedHtmlPage.data()).toStdString().data();
-		}
-		else
-		{
-			ERRORLOG << "Cannot find QTextCodec for page encoding";
-			ERRORLOG << "content-type:" << contentType;
-			ERRORLOG << "charset:" << charset;
-		}
-	}
-
-	return decodedHtmlPage;
 }
 
 }
