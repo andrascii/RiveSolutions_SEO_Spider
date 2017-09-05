43b8c13cba65ca3a1915dd001093a45650f7e8e5
Ярополк <kushnarev.eger@yandex.ru>
2017-09-05 08:00:49 +0300

    Создан кастомный комбо бокс для настроек

---
+++ quickiewebbot/include/control_adaptor_combo_box.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "icontrol_adaptor.h"
+
+namespace QuickieWebBot
+{
+
+	class ControlAdaptorQComboBox : public IControlAdaptor
+	{
+	public:
+		virtual void setControl(QObject* control) override;
+		virtual QVariant value() const override;
+		virtual void setValue(const QVariant& val) override;
+		virtual void connectChangesObserver(SettingsPage* page) override;
+
+	private:
+		QPointer<QComboBox> m_control;
+	};
+
+	Q_DECLARE_METATYPE(ControlAdaptorQComboBox);
+
+}
