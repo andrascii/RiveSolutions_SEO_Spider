190051dc0a3257900a679e41ab2d1117033a4e75
kirchet <kirchet@yandex.ru>
2017-10-03 22:05:56 +0300

    Дополнен тест на картинки (и он не проходит)

--- webcrawlertests/src/image_tests.cpp
+++ webcrawlertests/src/image_tests.cpp
@@ -8,7 +8,28 @@ TEST(ImageTests, EmptyAlt)
 	{
 		auto pages = cl->waitForParsedPageReceived(WebCrawler::MissingAltTextImageStorageType, 1, 10);
 		EXPECT_EQ(1, pages.size());
-		EXPECT_EQ(QString(""), pages[0]->altText);
+		EXPECT_EQ(1, pages[0]->linksToThisPage.size());
+		EXPECT_EQ(QString(""), pages[0]->linksToThisPage[0].altOrTitle);
+
+		WebCrawler::ParsedPagePtr sourcePage = pages[0]->linksToThisPage[0].resource.lock();
+		EXPECT_EQ(1, sourcePage->linksOnThisPage.size());
+		EXPECT_EQ(QString(""), sourcePage->linksOnThisPage[0].altOrTitle);
+	});
+}
+
+TEST(ImageTests, NoAlt)
+{
+	WebCrawlerTests::TestEnvironment env(TestEnvironment::defaultOptions(QUrl("http://image.com/no-alt.html")));
+	env.runTest([cl = env.crawler()]()
+	{
+		auto pages = cl->waitForParsedPageReceived(WebCrawler::MissingAltTextImageStorageType, 1, 10);
+		EXPECT_EQ(1, pages.size());
+		EXPECT_EQ(1, pages[0]->linksToThisPage.size());
+		EXPECT_EQ(QString(""), pages[0]->linksToThisPage[0].altOrTitle);
+
+		WebCrawler::ParsedPagePtr sourcePage = pages[0]->linksToThisPage[0].resource.lock();
+		EXPECT_EQ(1, sourcePage->linksOnThisPage.size());
+		EXPECT_EQ(QString(""), sourcePage->linksOnThisPage[0].altOrTitle);
 	});
 }
 
