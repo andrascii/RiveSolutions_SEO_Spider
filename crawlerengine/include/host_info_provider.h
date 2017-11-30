5dbbc7b78760247bf57af0cf3be3a7db2fbaa58e
andrey_ascii <pugachev.andrey.work@gmail.com>
2017-11-30 22:15:01 +0300

    Исправлен баг с ошибкой DNS

--- crawlerengine/include/host_info_provider.h
+++ crawlerengine/include/host_info_provider.h
@@ -7,14 +7,21 @@ namespace CrawlerEngine
 
 class HostInfoProvider : public QObject
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	HostInfoProvider();
-	~HostInfoProvider();
+    HostInfoProvider();
+    ~HostInfoProvider();
 
-	Q_INVOKABLE void handleRequest(RequesterSharedPtr requester);
-	Q_INVOKABLE void stopRequestHandling(RequesterSharedPtr requester);
+    Q_INVOKABLE void handleRequest(RequesterSharedPtr requester);
+    Q_INVOKABLE void stopRequestHandling(RequesterSharedPtr requester);
+
+#ifdef Q_OS_WIN
+
+private:
+    WSADATA m_wsadata;
+
+#endif
 };
 
 }
