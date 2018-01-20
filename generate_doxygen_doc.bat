a5e9d35c409ca3ded785d4939e2f61b25fffffb9
andrey_ascii <pugachev.andrey.work@gmail.com>
2018-01-20 18:25:26 +0300

    Настроено создание документации по коду с помощью doxygen. Используй скрипт generate_doxygen_doc.bat

--- generate_doxygen_doc.bat
+++ generate_doxygen_doc.bat
@@ -1,7 +1,5 @@
 @echo Generating doxygen documentation
 
-@set OUTPUT_DIRECTORY=%CD%\doxydoc
-@set DOXYGEN_EXE=doxygen.exe
+start /B doxygen.exe %CD%\doxyfile.dxg
 
-start /B doxygen.exe %CD%\doxyfile.dxg OUTPUT_DIRECTORY = %OUTPUT_DIRECTORY%
 @pause
