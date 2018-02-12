9221abbf09484e564bc071542ee75ab747d6b5e8
kirchet <kirchet@yandex.ru>
2018-02-12 21:31:09 +0300

    Убраны дубликаты одной и той же страницы с www и без

--- crawlerengine/src/url.cpp
+++ crawlerengine/src/url.cpp
@@ -17,6 +17,14 @@ const QString& Url::canonizedUrlStr() const
 		{
 			m_canonizedUrlStr = m_canonizedUrlStr.left(m_canonizedUrlStr.size() - 1);
 		}
+		if (m_canonizedUrlStr.startsWith(QString("http://www.")))
+		{
+			m_canonizedUrlStr = QString("http://") + m_canonizedUrlStr.mid(11);
+		}
+		else if (m_canonizedUrlStr.startsWith(QString("https://www.")))
+		{
+			m_canonizedUrlStr = QString("https://") + m_canonizedUrlStr.mid(12);
+		}
 	}
 
 	return m_canonizedUrlStr;
