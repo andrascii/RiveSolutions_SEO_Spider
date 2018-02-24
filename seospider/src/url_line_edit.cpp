7b253c2d66c5ebba26c6457285834817c53bb974
kirchet <kirchet@yandex.ru>
2018-02-24 16:16:21 +0300

    Поддержка урлов с русскими символами

--- seospider/src/url_line_edit.cpp
+++ seospider/src/url_line_edit.cpp
@@ -1,22 +1,61 @@
 #include "url_line_edit.h"
+#include "page_parser_helpers.h"
 
 namespace SeoSpider
 {
+
+class UrlValidator: public QValidator
+{
+public:
+	explicit UrlValidator(QObject* parent)
+		: QValidator(parent)
+	{
+	}
+
+	virtual State validate(QString& input, int& pos) const override
+	{
+		Q_UNUSED(pos);
+
+		static QRegularExpression s_urlRegExp(
+			"^(?:(?:https?):\\/\\/)?(?:\\S+(?::\\S*)?@|\\d{1,3}(?:\\.\\d{1,3}){3}|(?:(?:[a-z\\d\\x{00a1}-\\x{ffff}]+-?)*"
+			"[a-z\\d\\x{00a1}-\\x{ffff}]+)(?:\\.(?:[a-z\\d\\x{00a1}-\\x{ffff}]+-?)*[a-z\\d\\x{00a1}-\\x{ffff}]+)*"
+			"(?:\\.[a-z\\x{00a1}-\\x{ffff}]{2,6}))(?::\\d+)?(?:[^\\s]*)?$"
+		);
+
+		if (input.isEmpty())
+		{
+			return QValidator::Intermediate;
+		}
+
+		CrawlerEngine::Url url(input);
+		
+		if (url.isValid() && CrawlerEngine::PageParserHelpers::isHttpOrHttpsScheme(url))
+		{
+			if (!s_urlRegExp.match(input).hasMatch())
+			{
+				return QValidator::Intermediate;
+			}
+
+			if (url.scheme().isEmpty() || url.host().isEmpty())
+			{
+				input = QString("http://") + input;
+				pos += 7;
+			}
+
+			return QValidator::Acceptable;
+		}
+
+		return QValidator::Intermediate;
+	}
+};
+
 
 UrlLineEdit::UrlLineEdit(QWidget* parent) 
 	: QLineEdit(parent)
 	, m_isUrlCorrect(false)
 {
-	VERIFY(connect(this, SIGNAL(textEdited(const QString&)), this, SLOT(checkUrlCorrectness())));
-
-	QRegularExpression urlRegExp(
-			"^(?:(?:https?):\\/\\/)?(?:\\S+(?::\\S*)?@|\\d{1,3}(?:\\.\\d{1,3}){3}|(?:(?:[a-z\\d\\x{00a1}-\\x{ffff}]+-?)*"
-			"[a-z\\d\\x{00a1}-\\x{ffff}]+)(?:\\.(?:[a-z\\d\\x{00a1}-\\x{ffff}]+-?)*[a-z\\d\\x{00a1}-\\x{ffff}]+)*"
-			"(?:\\.[a-z\\x{00a1}-\\x{ffff}]{2,6}))(?::\\d+)?(?:[^\\s]*)?$"
-			);
-	
-	urlRegExp.setPatternOptions(QRegularExpression::CaseInsensitiveOption);
-	setValidator(new QRegularExpressionValidator(urlRegExp, this));
+	VERIFY(connect(this, SIGNAL(textChanged(const QString&)), this, SLOT(checkUrlCorrectness())));
+	setValidator(new UrlValidator(this));
 }
 
 void UrlLineEdit::checkUrlCorrectness()
