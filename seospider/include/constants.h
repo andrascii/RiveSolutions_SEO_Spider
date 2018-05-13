096abf7084cee6ee39b265cb43d22697c74f15a2
andrey_ascii <pugachev.andrey.work@gmail.com>
2018-05-13 20:42:06 +0300

    Изменено количество рабочих потоков

--- seospider/include/constants.h
+++ seospider/include/constants.h
@@ -6,7 +6,7 @@ namespace SeoSpider
 #ifndef PRODUCTION
 const unsigned int c_optimalParserThreadsCount = std::thread::hardware_concurrency() / 2;
 #else
-const unsigned int c_optimalParserThreadsCount = std::thread::hardware_concurrency() * 2;
+const unsigned int c_optimalParserThreadsCount = std::thread::hardware_concurrency() * 1.5;
 #endif
 
 const QByteArray c_projectFileExtension = ".ssproj";
