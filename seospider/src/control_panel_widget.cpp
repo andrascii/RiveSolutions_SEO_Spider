1de0a0c9a8295cdc865014fb135a66d5b56e6917
Ярополк <kushnarev.eger@yandex.ru>
2018-08-04 19:08:35 +0300

    FIXED: Second "Enter" key button for starting crawler does not work

--- seospider/src/control_panel_widget.cpp
+++ seospider/src/control_panel_widget.cpp
@@ -46,7 +46,8 @@ bool ControlPanelWidget::eventFilter(QObject* object, QEvent* event)
 
 	QKeyEvent* keyEvent = static_cast<QKeyEvent*>(event);
 
-	if (keyEvent->key() != Qt::Key_Return)
+	if (keyEvent->key() != Qt::Key_Return 
+		&& keyEvent->key() != Qt::Key_Enter)
 	{
 		return false;
 	}
