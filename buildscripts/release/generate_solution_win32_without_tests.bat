c5d0efd124bf64bdd92c8da0ac1ff1af2a54af28
Pugachev Andrey <pugachev.andrey.work@gmail.com>
2018-12-20 00:34:04 -0800

    Updated build scripts in order to use only bat scripts. There is no need now to set some environment variables manually

--- buildscripts/release/generate_solution_win32_without_tests.bat
+++ buildscripts/release/generate_solution_win32_without_tests.bat
@@ -1,14 +1,10 @@
+set SS_PLATFORM=win32
+call "../details/prepare_environment.bat"
 cd ..
 cd ..
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Buildvcvars32.bat"
 mkdir build
 cd build
 del CMakeCache.txt
-set SS_PLATFORM=win32
-set CMAKE_PREFIX_PATH=%SS_DEPS%\msvc_2017_%SS_PLATFORM%
-set BOOST_LIBRARYDIR=%SS_DEPS%\boost\%SS_PLATFORM%\lib
-set BOOST_ROOT=%SS_DEPS%\boost
-set path=%SS_DEPS%\icu\icu_%SS_PLATFORM%\lib;%path%
 set TESTS=disabled
 cmake -G "Visual Studio 15 2017" .. -DMSVC_RUNTIME=dynamic -DCMAKE_BUILD_TYPE=Release
 cd ..
--- buildscripts/release/generate_solution_x64.bat
+++ buildscripts/release/generate_solution_x64.bat
@@ -1,14 +1,10 @@
+set SS_PLATFORM=x64
+call "../details/prepare_environment.bat"
 cd ..
 cd ..
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Buildvcvars64.bat"
 mkdir build
 cd build
 del CMakeCache.txt
-set SS_PLATFORM=x64
-set CMAKE_PREFIX_PATH=%SS_DEPS%\msvc_2017_%SS_PLATFORM%
-set BOOST_LIBRARYDIR=%SS_DEPS%\boost\%SS_PLATFORM%\lib
-set BOOST_ROOT=%SS_DEPS%\boost
-set path=%SS_DEPS%\icu\icu_%SS_PLATFORM%\lib;%path%
 set TESTS=enabled
 cmake -G "Visual Studio 15 2017 Win64" .. -DMSVC_RUNTIME=dynamic -DCMAKE_BUILD_TYPE=Release
 cd ..
