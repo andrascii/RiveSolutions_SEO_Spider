ae2eee468bd175baa67a9182f37adcbd7ea7332f
Pugachev Andrey <pugachev.andrey.work@gmail.com>
2018-12-20 22:03:27 -0800

    Removed set path in the build scripts

--- buildscripts/details/prepare_environment.bat
+++ buildscripts/details/prepare_environment.bat
@@ -2,5 +2,4 @@ set SS_DEPS=D:\deps
 set CMAKE_PREFIX_PATH=%SS_DEPS%\qt\msvc_2017_%SS_PLATFORM%
 set BOOST_LIBRARYDIR=%SS_DEPS%\boost\%SS_PLATFORM%\lib
 set BOOST_ROOT=%SS_DEPS%\boost
-set path=%SS_DEPS%\qt\msvc_2017_%SS_PLATFORM%\bin;%SS_DEPS%\qt\msvc_2017_%SS_PLATFORM%\plugins\platforms;%SS_DEPS%\openssl\openssl_%SS_PLATFORM%\bin;%SS_DEPS%\icu\icu_%SS_PLATFORM%\lib;%path%
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
