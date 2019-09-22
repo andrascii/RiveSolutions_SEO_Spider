c691f80ef466d00afc021132d753374df764d2fb
Pugachev Andrey <apugachevdev@gmail.com>
2019-09-22 16:46:26 +0300

    renamed build scripts according to target IDE name

---
+++ buildscripts/release/vs2017_win32.bat
@@ -0,0 +1,11 @@
+set SS_PLATFORM=win32
+call "../details/prepare_environment.bat"
+cd ..
+cd ..
+mkdir build
+cd build
+del CMakeCache.txt
+set TESTS=enabled
+cmake -G "Visual Studio 15 2017" .. -DMSVC_RUNTIME=dynamic -DCMAKE_BUILD_TYPE=Release
+cd ..
+cd ./buildscripts/release/
